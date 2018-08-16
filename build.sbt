1615660de2909f03f79b05cd36b0e31556af2ec5
Vit Brunner <vit.brunner@gmail.com>
2018-08-16 18:57:37 +0200

    Options

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "0.1"
 scalaVersion := "2.12.6"
 
 libraryDependencies ++= Seq(
+  "com.github.scopt" %% "scopt" % "3.7.0",
   "io.monix" %% "monix" % "3.0.0-RC1",
   "org.scalatest" %% "scalatest" % "3.0.5" % Test
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,11 @@
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
+import scopt.OptionParser
+
 object Main {
+  case class Args(timeout: Int = 60)
+
   private def printResults(results: Seq[Result]): Unit = {
     results.sortBy(_.elapsed).foreach(println)
 
@@ -12,10 +16,28 @@ object Main {
     println("Bytes read per second: " + overall.bytes / (overall.elapsed / 1000))
   }
 
-  def main(args: Array[String]): Unit = {
-    val timeout = 60
+  private def run(timeout: Int): Unit = {
+    val results = Await.result(
+      Parallel.run(timeout),
+      (timeout + 10).seconds
+    )
 
-    val results = Await.result(Parallel.run(timeout), (timeout * 2).seconds)
     printResults(results)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val parser = new OptionParser[Args]("leapfin") {
+      opt[Int]('t', "timeout")
+        .action((x, c) => c.copy(timeout = x))
+        .text("timeout in seconds")
+
+      help("help")
+        .text("prints this usage text")
+    }
+
+    parser.parse(args, Args()) match {
+      case Some(config) => run(config.timeout)
+      case None => Unit
+    }
   }
 }
