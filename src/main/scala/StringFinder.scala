02e9d39817defb0d756e8c296e98cfec0b140fbf
Vit Brunner <vit.brunner@gmail.com>
2018-08-16 11:58:04 +0200

    Include time to find

--- src/main/scala/StringFinder.scala
+++ src/main/scala/StringFinder.scala
@@ -1,24 +1,29 @@
 import scala.annotation.tailrec
 
-case class Result(elapsed: Int, bytes: Int, status: String)
+case class Result(elapsed: Long, bytes: Int, status: String)
 
 object StringFinder {
-  val needle: String = "Lp"
+  val needle: String = "Lpfn"
 
   def find(stream: Stream[Char]): Result =
-    findNeedle(stream, 0, 0)
+    findNeedle(stream, 0, 0, System.currentTimeMillis())
 
   // we assume:
   // - the stream is infinite
   // - each of the characters of the needle is unique
   @tailrec
-  private def findNeedle(stream: Stream[Char], found: Int, count: Int): Result =
+  private def findNeedle(
+    stream: Stream[Char],
+    found: Int,
+    count: Int,
+    startedAt: Long
+  ): Result =
     if (found == needle.length)
-      Result(0, count, "SUCCESS")
+      Result(System.currentTimeMillis() - startedAt, count, "SUCCESS")
     else if (stream.head == needle(found))
-      findNeedle(stream.tail, found + 1, count + 1)
+      findNeedle(stream.tail, found + 1, count + 1, startedAt)
     else if (stream.head == needle(0))
-      findNeedle(stream.tail, 1, count + 1)
+      findNeedle(stream.tail, 1, count + 1, startedAt)
     else
-      findNeedle(stream.tail, 0, count + 1)
+      findNeedle(stream.tail, 0, count + 1, startedAt)
 }
